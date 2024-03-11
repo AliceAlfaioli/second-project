be1b79269b21ba1af1eb9e2721087241fd0581e1
AliceAlfaioli <156820194+AliceAlfaioli@users.noreply.github.com>
2024-03-11 16:54:07 +0100

    stile css

--- src/App.js
+++ src/App.js
@@ -7,17 +7,18 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <ButtonComponent />
+        <ButtonComponent className="button" />
 
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Esercizio App React</p>
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
 
-        <ImageComponent src="https://images.unsplash.com/photo-1708649290066-5f617003b93f?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+        <ImageComponent
+          src="https://images.unsplash.com/photo-1708649290066-5f617003b93f?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt="Acquamarina"
+        />
       </header>
     </div>
   );
